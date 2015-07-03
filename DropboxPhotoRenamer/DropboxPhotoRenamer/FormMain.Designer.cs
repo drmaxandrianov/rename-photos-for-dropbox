fdcf5a0afe45eaa415b15b974f93810d4dd7e08a
m.pestun <m.pestun@corp.mail.ru>
2015-07-03 13:26:07 +0300

    version 1

---
+++ DropboxPhotoRenamer/DropboxPhotoRenamer/FormMain.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace DropboxPhotoRenamer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.ButtonSelectFolder = new System.Windows.Forms.Button();
+            this.ButtonRename = new System.Windows.Forms.Button();
+            this.ProgressBar = new System.Windows.Forms.ProgressBar();
+            this.BackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // FolderBrowserDialog
+            // 
+            this.FolderBrowserDialog.ShowNewFolderButton = false;
+            // 
+            // ButtonSelectFolder
+            // 
+            this.ButtonSelectFolder.Location = new System.Drawing.Point(13, 13);
+            this.ButtonSelectFolder.Name = "ButtonSelectFolder";
+            this.ButtonSelectFolder.Size = new System.Drawing.Size(277, 23);
+            this.ButtonSelectFolder.TabIndex = 0;
+            this.ButtonSelectFolder.Text = "Choose...";
+            this.ButtonSelectFolder.UseVisualStyleBackColor = true;
+            this.ButtonSelectFolder.Click += new System.EventHandler(this.ButtonSelectFolder_Click);
+            // 
+            // ButtonRename
+            // 
+            this.ButtonRename.Location = new System.Drawing.Point(12, 42);
+            this.ButtonRename.Name = "ButtonRename";
+            this.ButtonRename.Size = new System.Drawing.Size(277, 23);
+            this.ButtonRename.TabIndex = 1;
+            this.ButtonRename.Text = "Rename";
+            this.ButtonRename.UseVisualStyleBackColor = true;
+            this.ButtonRename.Click += new System.EventHandler(this.ButtonRename_Click);
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Location = new System.Drawing.Point(12, 71);
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(276, 23);
+            this.ProgressBar.TabIndex = 2;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 108);
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.ButtonRename);
+            this.Controls.Add(this.ButtonSelectFolder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Dropbox Photo Renamer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog FolderBrowserDialog;
+        private System.Windows.Forms.Button ButtonSelectFolder;
+        private System.Windows.Forms.Button ButtonRename;
+        private System.Windows.Forms.ProgressBar ProgressBar;
+        private System.ComponentModel.BackgroundWorker BackgroundWorker;
+    }
+}
+
---
+++ DropboxPhotoRenamer/DropboxPhotoRenamer/FormMain.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DropboxPhotoRenamer
+{
+    public partial class FormMain : Form
+    {
+        private string _rootFolderPath = "";
+
+        public FormMain()
+        {
+            InitializeComponent();
+            BackgroundWorker.WorkerReportsProgress = true;
+            BackgroundWorker.DoWork += BackgroundWorker_DoWork;
+            BackgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+            BackgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+        }
+
+        void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ButtonSelectFolder.Enabled = true;
+            ButtonRename.Enabled = true;
+        }
+
+        void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ProgressBar.Value = e.ProgressPercentage;
+        }
+
+        void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var files = Directory.GetFiles(_rootFolderPath);
+
+            for (var i = 0; i < files.Length; i++)
+            {
+                // Photo 20-05-15 15 51 02.jpg -> 2015-03-20 11.53.05.jpg
+                try { 
+                    var fileName = Path.GetFileNameWithoutExtension(files[i]);
+                    var filePath = files[i].Replace(Path.GetFileName(files[i]), "");
+                    var parts = fileName.Split(new char[]{' ', '-', '.'});
+                    var newName = "20" + parts[3] + "-" + parts[2] + "-" + parts[1];
+                    newName += " " + parts[4] + "." + parts[5] + "." + parts[6];
+
+
+                    Console.WriteLine(files[i] + "\t - \t" + filePath + newName);
+                    try
+                    {
+                        File.Move(files[i], filePath + newName + Path.GetExtension(files[i]));
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            File.Move(files[i], filePath + newName + " duplicated" + Path.GetExtension(files[i]));
+                        } catch {}
+                    }
+                } catch {
+                    continue;
+                }
+
+                BackgroundWorker.ReportProgress(i + 1);
+            }
+        }
+
+        private void ButtonSelectFolder_Click(object sender, EventArgs e)
+        {
+            var result = FolderBrowserDialog.ShowDialog(this);
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                _rootFolderPath = FolderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void ButtonRename_Click(object sender, EventArgs e)
+        {
+            if (_rootFolderPath == "")
+            {
+                MessageBox.Show(this, "Select folder with files!", "Select Folder", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            var files = Directory.GetFiles(_rootFolderPath);
+
+            ProgressBar.Value = 0;
+            ProgressBar.Maximum = files.Length;
+
+            ButtonSelectFolder.Enabled = false;
+            ButtonRename.Enabled = false;
+
+            BackgroundWorker.RunWorkerAsync();
+        }
+    }
+}
